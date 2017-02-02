5c75fb5a07d0ada1be4187fcf74e0f4eda509f45
Priyanka Seth <priyanka.seth@babylonhealth.com>
2017-02-02 15:56:38 +0000

    Repacked node2vec
    -- introduce setup.py to install completely
    -- change file names to comply with python setup
    -- now writes script "node2vec" to directly use from command line
    -- now allows interactive use by creating and modifying args, and passing to main

---
+++ setup.py
@@ -0,0 +1,90 @@
+from setuptools import setup, find_packages
+from codecs import open
+import os
+import io
+import re
+
+__pkg_name__ = 'node2vec'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+setup(
+    name=__pkg_name__,
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=find_version(__pkg_name__, "__init__.py"),
+
+    description='Reference implementation of node2vec',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/aditya-grover/node2vec',
+
+    # Author details
+    author='Aditya Grover and Jure Leskovec',
+    author_email='adityag@cs.stanford.edu',
+
+    # Choose your license
+    license='MIT',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['networkx>=1.11','numpy>=1.11.2','gensim>=0.13.3'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={},
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={},
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'node2vec = node2vec.main:main',
+        ],
+    },
+)
